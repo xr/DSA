0f43cbba2f3d04b5174ffb8ea9bdf7a8c1ad1e6a
liang <guoliang1222@gmail.com>
2016-10-08 14:06:58 +0300

    modify test file

--- algorithms/sort/test.py
+++ algorithms/sort/test.py
@@ -19,61 +19,68 @@ class TestUtils(unittest.TestCase):
 
 # compare with the built in python sort function
 class TestPythonBuiltInSort(unittest.TestCase):
+    def test_python_build_in_sort_func_correctness(self):
+        a = [3,5,2,4,8,7,10,43,1,9]
+        start_time = time.time()
+        result = sorted(a)
+        print("##Python Sort <10> ## ==> TIME: %s seconds\n" % (time.time() - start_time))
+        self.assertEqual(result, [1,2,3,4,5,7,8,9,10,43])
+
     def test_python_build_in_sort_func_small_collection(self):
         start_time = time.time()
         result = sorted(SMALL_COLLECTION)
-        print("##Python Sort SC## ==> TIME: %s seconds\n" % (time.time() - start_time))
+        print("##Python Sort <1000>## ==> TIME: %s seconds\n" % (time.time() - start_time))
         self.assertEqual(result, SMALL_COLLECTION_RESULT)
 
 class TestBubbleSort(unittest.TestCase):
     def test_bubbleSort_correctness(self):
-        a = [3,5,2,4,8,7,10]
+        a = [3,5,2,4,8,7,10,43,1,9]
 
         start_time = time.time()
         result = bubbleSort(a)
-        print("##Bubble Sort C ## ==> TIME: %s seconds\n" % (time.time() - start_time))
+        print("##Bubble Sort <10> ## ==> TIME: %s seconds\n" % (time.time() - start_time))
 
-        self.assertEqual(result, [2,3,4,5,7,8,10])
+        self.assertEqual(result, [1,2,3,4,5,7,8,9,10,43])
 
     def test_bubbleSort_small_collection(self):
         start_time = time.time()
         result = bubbleSort(SMALL_COLLECTION)
-        print("##Bubble Sort SC ## ==> TIME: %s seconds\n" % (time.time() - start_time))
+        print("##Bubble Sort <1000> ## ==> TIME: %s seconds\n" % (time.time() - start_time))
         self.assertEqual(result, SMALL_COLLECTION_RESULT)
         
 
 
 class TestSelectionSort(unittest.TestCase):
     def test_selectionSort_correctness(self):
-        a = [3,5,2,4,8,7,10]
+        a = [3,5,2,4,8,7,10,43,1,9]
 
         start_time = time.time()
         result = selectionSort(a)
-        print("##Selection Sort C ## ==> TIME: %s seconds\n" % (time.time() - start_time))
+        print("##Selection Sort <10> ## ==> TIME: %s seconds\n" % (time.time() - start_time))
 
-        self.assertEqual(result, [2,3,4,5,7,8,10])
+        self.assertEqual(result, [1,2,3,4,5,7,8,9,10,43])
 
     def test_selectionSort_small_collection(self):
         start_time = time.time()
         result = selectionSort(SMALL_COLLECTION)
-        print("##Selection Sort SC ## ==> TIME: %s seconds\n" % (time.time() - start_time))
+        print("##Selection Sort <1000> ## ==> TIME: %s seconds\n" % (time.time() - start_time))
         self.assertEqual(result, SMALL_COLLECTION_RESULT)
 
 
 class TestInsertionSort(unittest.TestCase):
     def test_insertionSort_correctness(self):
-        a = [3,5,2,4,8,7,10]
+        a = [3,5,2,4,8,7,10,43,1,9]
 
         start_time = time.time()
         result = insertionSort(a)
-        print("##Insertion Sort C ## ==> TIME: %s seconds\n" % (time.time() - start_time))
+        print("##Insertion Sort <10> ## ==> TIME: %s seconds\n" % (time.time() - start_time))
 
-        self.assertEqual(result, [2,3,4,5,7,8,10])
+        self.assertEqual(result, [1,2,3,4,5,7,8,9,10,43])
 
     def test_insertionSort_small_collection(self):
         start_time = time.time()
         result = insertionSort(SMALL_COLLECTION)
-        print("##Insertion Sort SC ## ==> TIME: %s seconds\n" % (time.time() - start_time))
+        print("##Insertion Sort <1000> ## ==> TIME: %s seconds\n" % (time.time() - start_time))
         self.assertEqual(result, SMALL_COLLECTION_RESULT)
 
 if __name__ == '__main__':
