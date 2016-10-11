430df8a8daedcd4dad86b4f8bc2621581e23a64f
liang <guoliang1222@gmail.com>
2016-10-11 21:14:50 +0300

    update insertion sort implementation

--- algorithms/sort/insertion.py
+++ algorithms/sort/insertion.py
@@ -3,10 +3,16 @@ from utils import swap
 def insertionSort(arr):
 	position = 1
 	for position in range(len(arr)):
-		for j in range(0, position):
-			if arr[position] < arr[j]:
-				temp = arr[position]
-				arr.remove(arr[position])
-				arr.insert(j, temp)
-		position += 1
+		# EDIT: avoid the following implementation
+		# if so, the best case is still O(n^2)
+		# for j in range(0, position):
+		# 	if arr[position] < arr[j]:
+		# 		temp = arr[position]
+		# 		arr.remove(arr[position])
+		# 		arr.insert(j, temp)
+		# position += 1
+		j = position
+		while ( j != 0) and (arr[j] < arr[j - 1]):
+			swap(j, j - 1, arr)
+			j -= 1
 	return arr
